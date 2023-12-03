fcb0ac4f2235ffa65dbb2cee7b15bb38ccd13eaa
Navin Avadhani <knavinavadhani@gmail.com>
2023-12-03 16:14:30 +0530

    extra: orders list skeleton designed for future use.

--- app/(routes)/dashboard/orders/page.tsx
+++ app/(routes)/dashboard/orders/page.tsx
@@ -1,5 +1,6 @@
 import { OrderStatus } from "@prisma/client";
 
+import { Skeleton } from "~/components/ui/skeleton";
 import OrdersList from "~/components/orders-page/orders-list";
 import OrdersFilter from "~/components/orders-page/orders-filter";
 import OrderPaymentPanel from "~/components/orders-page/order-payment-panel";
@@ -32,3 +33,30 @@ export default async function OrdersPage({ searchParams }: Props) {
     </article>
   );
 }
+
+// function OrdersListSkeleton() {
+//   return (
+//     <div className="space-y-6">
+//       {Array(3)
+//         .fill(null)
+//         .map((_, idx) => (
+//           <div key={idx} className="space-y-6">
+//             <div className="grid flex-1 grid-cols-2 rounded-2xl bg-white p-4">
+//               <div className="space-y-1">
+//                 <Skeleton className="h-7 w-[12.5rem]" />
+//                 <Skeleton className="h-4 w-20" />
+//                 <Skeleton className="h-5 w-14" />
+//               </div>
+//               <div className="flex flex-col items-end justify-start gap-y-4">
+//                 <Skeleton className="h-7 w-[4.5rem]" />
+//                 <div className="flex items-center gap-x-4">
+//                   <Skeleton className="h-7 w-16" />
+//                   <Skeleton className="h-6 w-16 rounded-full" />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//     </div>
+//   );
+// }
